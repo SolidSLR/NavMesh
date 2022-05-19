8d4345a9e001fcc3f6c31e37559d0c8e9b784f54
Santiago López Rey <devsantilopez@gmail.com>
2022-05-19 20:21:57 +0200

    NPC persigue en ángulo concreto

--- Assets/Scripts/SlowNPC.cs
+++ Assets/Scripts/SlowNPC.cs
@@ -9,7 +9,8 @@ public class SlowNPC : MonoBehaviour
     private Vector3 initTransf;
     NavMeshAgent agent;
     public float maxDistance = 7f;
-
+    public Quaternion leftAngle;
+    public Quaternion rightAngle;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,9 +21,9 @@ public class SlowNPC : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-       if(CanChase2()){
+       if(CanChase3()){
            agent.destination = preyNPC.position;
-       }else if(!CanChase2()){
+       }else if(!CanChase3()){
            agent.destination = initTransf;
        }
     }
@@ -39,7 +40,8 @@ public class SlowNPC : MonoBehaviour
 
     public bool CanChase2(){
                 
-        if(Physics.Raycast(transform.position, (preyNPC.position-transform.position).normalized, out RaycastHit hitInfo ,maxDistance, -1, QueryTriggerInteraction.Ignore)){
+        if(Physics.Raycast(transform.position+Vector3.up*0.1f, (preyNPC.position-transform.position).normalized,
+        out RaycastHit hitInfo ,maxDistance, -1, QueryTriggerInteraction.Ignore)){
             Debug.Log("Objecto encontrado: "+hitInfo.collider.gameObject.name);
             Debug.Log("CanChase2(): Objetivo encontrado");
            if(hitInfo.collider.gameObject.tag == "NPC"){
@@ -50,6 +52,28 @@ public class SlowNPC : MonoBehaviour
         }else{
            Debug.Log("CanChase2(): No hay objetivo");
            return false;
+       }
+    }
+
+    public bool CanChase3(){
+
+        if(Physics.Raycast(transform.position+Vector3.up*0.1f, (preyNPC.position-transform.position).normalized,
+        out RaycastHit hitInfo, maxDistance, -1, QueryTriggerInteraction.Ignore)){
+            
+            if(leftAngle.eulerAngles.magnitude <= hitInfo.collider.transform.eulerAngles.magnitude &&
+             hitInfo.collider.transform.eulerAngles.magnitude >= leftAngle.eulerAngles.magnitude){
+
+                if(hitInfo.collider.gameObject.tag == "NPC"){
+                    return true;
+                }else{
+                    return false;
+                }
+            }else{ 
+                return false;
+                }
+        }else{
+            Debug.Log("CanChase2(): No hay objetivo");
+            return false;
        }
     }
 }
