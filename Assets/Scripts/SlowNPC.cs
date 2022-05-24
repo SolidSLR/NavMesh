abb18c13334b5b5271144d08801f25b305f7a3cc
Santiago López Rey <devsantilopez@gmail.com>
2022-05-24 20:25:11 +0200

    Engadidos estados para cambio de rutina

--- Assets/Scripts/SlowNPC.cs
+++ Assets/Scripts/SlowNPC.cs
@@ -6,36 +6,61 @@ using UnityEngine.AI;
 public class SlowNPC : MonoBehaviour
 {
     public Transform preyNPC;
-    private Vector3 initTransf;
+    private Vector3 initPos;
     NavMeshAgent agent;
+    AgentState state;
     public float maxDistance = 7f;
     public Quaternion leftAngle;
     public Quaternion rightAngle;
     // Start is called before the first frame update
     void Start()
     {
-        initTransf = transform.position;
+        state = AgentState.Idle;
+        initPos = transform.position;
         agent = GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
     void Update()
     {
-       if(CanChase3()){
+       /*if(CanChase3()){
            agent.destination = preyNPC.position;
        }else if(!CanChase3()){
            agent.destination = initTransf;
-       }
-
-       Debug.DrawRay(transform.position, transform.forward*10f, Color.red, 1f);
+       }*/
+       bool canChase = CanChase3();
+       switch(state){
+            case AgentState.Idle:
+            if(canChase){
+                state = AgentState.Chasing;
+            }
+            break;
+            case AgentState.Chasing:
+            if(canChase){
+                agent.destination = preyNPC.position;
+            }else{
+                state = AgentState.Returning;
+            }
+            break;
+            case AgentState.Returning:
+            if(canChase){
+                state = AgentState.Chasing;
+            }else{
+                agent.destination = initPos;
+            }
+            break;
+        }
+        Debug.Log("Estado actual: "+state.ToString());
+        Debug.Log("Valor de canChase "+canChase.ToString());
+        Debug.DrawRay(transform.position, transform.forward*10f, Color.red, 1f);
     }
 
     public bool CanChase(){
         if(Physics.Raycast(transform.position, (preyNPC.position-transform.position).normalized, maxDistance)){
-            Debug.Log("CanChase(): Objetivo encontrado");
+            //Debug.Log("CanChase(): Objetivo encontrado");
            return true;
        }else{
-           Debug.Log("CanChase(): No hay objetivo");
+           //Debug.Log("CanChase(): No hay objetivo");
            return false;
        }
     }
@@ -44,15 +69,15 @@ public class SlowNPC : MonoBehaviour
                 
         if(Physics.Raycast(transform.position+Vector3.up*0.1f, (preyNPC.position-transform.position).normalized,
         out RaycastHit hitInfo ,maxDistance, -1, QueryTriggerInteraction.Ignore)){
-            Debug.Log("Objecto encontrado: "+hitInfo.collider.gameObject.name);
-            Debug.Log("CanChase2(): Objetivo encontrado");
+            //Debug.Log("Objecto encontrado: "+hitInfo.collider.gameObject.name);
+            //Debug.Log("CanChase2(): Objetivo encontrado");
            if(hitInfo.collider.gameObject.tag == "NPC"){
                return true;
            }else{
                return false;
            }
         }else{
-           Debug.Log("CanChase2(): No hay objetivo");
+           //Debug.Log("CanChase2(): No hay objetivo");
            return false;
        }
     }
@@ -61,7 +86,7 @@ public class SlowNPC : MonoBehaviour
 
         Vector3 targetDir = preyNPC.position - transform.position;
         float angle = Vector3.Angle(targetDir, transform.forward);
-        Debug.Log("Ángulo respecto a target "+angle);
+        //Debug.Log("Ángulo respecto a target "+angle);
         if(Physics.Raycast(transform.position+Vector3.up*0.1f, (preyNPC.position-transform.position).normalized,
         out RaycastHit hitInfo, maxDistance, -1, QueryTriggerInteraction.Ignore)){
 
@@ -77,5 +102,23 @@ public class SlowNPC : MonoBehaviour
         }else{
             return false;
         }
+    }
+    void SetState(AgentState newState){
+        if(state != newState){
+            switch(newState){
+                case AgentState.Idle:
+                break;
+                case AgentState.Chasing:
+                break;
+                case AgentState.Returning:
+                transform.position = initPos;
+                break;
+            }
+        }
+    }
+    public enum AgentState{
+        Idle,
+        Chasing,
+        Returning
     }
 }
